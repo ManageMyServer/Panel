e28aea7d6f3f1823cebe217e170477c0076d870f
BrightSkyz <BrightSkyz@users.noreply.github.com>
2017-04-23 21:54:43 -0400

    Update index.php

--- index.php
+++ index.php
@@ -8,4 +8,13 @@
     if ($debug == true) {
         echo $_GET['page'];
     }
+    if ($_GET['page'] == '')
+        header('Location: ?page=login');
+        exit();
+    }
+    if (file_exists('pages/' . $_GET['page'] . '.php') {
+        include 'pages/' . $_GET['page'] . '.php';
+    } else {
+        include 'pages/errors/404.php';
+    }
 ?>
