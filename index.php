88ff14eb297087f36bc1755ad33f157db89ef468
BrightSkyz <BrightSkyz@users.noreply.github.com>
2017-04-23 21:23:15 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    // --------------------
+    // (C) ManageMyServer
+    // Website: Coming Soon
+    // Docs: Coming Soon
+    // --------------------
+    $debug = false
+    if ($debug == true) {
+        echo $_GET['page'];
+    }
+?>
